a0b730763e7dbb883afe7ffb8d441a5c5189e2f3
izznatsir <natsirizzuddin@gmail.com>
2019-03-27 14:43:22 +0700

    Topbar Component

---
+++ kabobs_frontend/src/components/Navscreen/Navscreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './Navscreen.module.css';
+import logo from './logo-original-min.png';
+
+const Topbar = () => {
+
+	return (
+		<div className={css.navscreen}>
+			
+      </div>
+	)
+}
+
+export default Topbar;
