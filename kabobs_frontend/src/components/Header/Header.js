abe7c11db687e879b8e98c2f7b57f1b0edc28ecc
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-03-27 07:32:39 +0100

    Header template
    Header Template

---
+++ kabobs_frontend/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Header =()=>{
+    return(
+        <div>
+            <p>
+                {'This is kabobs website'}
+            </p>
+            <div>
+                
+                <h1>Kabobs</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
